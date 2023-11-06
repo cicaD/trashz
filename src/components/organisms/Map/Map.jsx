2950c76d919f0685d58fd11b8995375a4418d787
Dragan Spiric <dragan.spiric@folksam.se>
2023-11-06 19:59:03 +0100

    Added styles and enabled get my location button on map

--- src/components/organisms/Map/Map.jsx
+++ src/components/organisms/Map/Map.jsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
 import CustomButton from './CustomButton';
+import "./Map.css"
 import "leaflet/dist/leaflet.css"
 const Map = () => {
     const [mapCenter, setMapCenter] = useState([47.46863, 19.15359]);
@@ -52,7 +53,7 @@ const Map = () => {
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
-            <CustomButton onClick={getCurrentLocation} text="Pick my location" />
+            <CustomButton className="custom-button" onClick={getCurrentLocation} text="Pick my location" />
             <MarkerHandler visible={markerPosition} position={markerPosition} />
 
 
